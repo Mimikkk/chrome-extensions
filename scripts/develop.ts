ae689480e1a095a64943cc70c8c3becc143892cf
Hououin Kyouma <inwazzin@gmail.com>
2023-11-19 16:41:05 +0100

    upgrade develop script

--- scripts/develop.ts
+++ scripts/develop.ts
@@ -1,29 +1,42 @@
 import chokidar from "chokidar";
-import * as child_process from "child_process";
+import child_process from "child_process";
 import { readArgument } from "./shared/readArgument.js";
 import { debounce } from "./shared/debounce.js";
-import * as fs from "fs";
+import fs from "fs";
 
 const argument = readArgument("name", "all");
 const names = argument === "all" ? fs.readdirSync("src/plugins").filter((name) => name !== "template") : [argument];
 
-console.log('watching "src" for changes.');
-console.log(`Developing ${names.map((name) => `'${name}'`).join(", ")}.`);
+console.log(`Developing: ${names.map((name) => `'${name}'`).join(", ")}.`);
 
-names.forEach((name) => {
-  let process: child_process.ChildProcess | undefined;
+const cache = new Map<string, child_process.ChildProcess>();
+const build = (name: string) => {
+  console.log(`-- Building plugin '${name}'.`);
 
-  chokidar.watch("src").on(
-    "change",
-    debounce(() => {
-      console.log(`Building plugin '${name}'.`);
-      if (process) process.kill("SIGKILL");
+  let process = cache.get(name);
+  if (process) {
+    console.log(`-- Killing previous build.`);
+    process.kill("SIGKILL");
+  }
 
-      process = child_process.exec(`pnpm run build -- --name=${name}`, (error, stdout, stderr) => {
-        if (error) console.error(error);
-        if (stdout) console.log(stdout);
-        if (stderr) console.error(stderr);
-      });
+  cache.set(
+    name,
+    child_process.exec(`pnpm run build -- --name=${name}`, (error, stdout, stderr) => {
+      if (error) console.error(error);
+      if (stdout) console.log(stdout);
+      if (stderr) console.error(stderr);
     }),
   );
+};
+
+names.forEach((name) => {
+  console.log(`- watching src/${name}/ for changes.`);
+
+  chokidar.watch(`src/${name}`).on(
+    "change",
+    debounce(() => build(name)),
+  );
 });
+
+console.log(`- watching src/shared/ for changes.`);
+chokidar.watch("src/shared").on("change", () => names.forEach((name) => build(name)));
